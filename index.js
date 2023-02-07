d1d9f793a62de69394c31264689a20e4001d85df
Leonardotorre18 <leonardotorre14@gmail.com>
2023-02-07 14:12:39 -0400

    Added endpoint

--- index.js
+++ index.js
@@ -2,4 +2,7 @@ const express = require('express');
 
 const app = express()
 app.use(express.static('./public'))
+app.get('/api' , (req, res) => {
+  res.json({message: 'Runnign '})
+})
 app.listen(3000, () => console.log('api runnin'))
